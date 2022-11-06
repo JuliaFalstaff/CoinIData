e511392633726e673a41dc8bd4e9d8298e970586
Julia Kholstinina <j.kholstinina@yandex.ru>
2022-11-06 19:00:52 +0300

    add UseCases mockito test

--- app/src/main/java/com/example/currencycryptoapp/di/DataModule.kt
+++ app/src/main/java/com/example/currencycryptoapp/di/DataModule.kt
@@ -13,7 +13,6 @@ import dagger.Provides
 
 @Module
 interface DataModule {
-
     @Binds
     fun bindCoinRepository(impl: CoinRepositoryImpl): CoinRepository
 
---
+++ app/src/test/java/com/example/currencycryptoapp/GetCoinInfoDetailsUseCaseTest.kt
@@ -0,0 +1,29 @@
+package com.example.currencycryptoapp
+
+import com.example.currencycryptoapp.domain.CoinRepository
+import com.example.currencycryptoapp.domain.GetCoinInfoDetailsUseCase
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+class GetCoinInfoDetailsUseCaseTest {
+
+    @Mock
+    lateinit var repository: CoinRepository
+    lateinit var getCoinInfoDetailsUseCase: GetCoinInfoDetailsUseCase
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        getCoinInfoDetailsUseCase = GetCoinInfoDetailsUseCase(repository)
+    }
+
+    @Test
+    fun invokeTest() {
+        val symbol = "BTC"
+        getCoinInfoDetailsUseCase.invoke(symbol)
+        Mockito.verify(repository, Mockito.times(1)).getCoinInfoDetails(symbol)
+    }
+}
