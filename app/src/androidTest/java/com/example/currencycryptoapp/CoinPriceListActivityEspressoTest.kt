1258e7b468bef654a4d4f82c50e3bd1f1ec2f49c
Julia Kholstinina <j.kholstinina@yandex.ru>
2022-11-07 22:29:21 +0300

    add some test for activity

---
+++ app/src/androidTest/java/com/example/currencycryptoapp/CoinPriceListActivityEspressoTest.kt
@@ -0,0 +1,41 @@
+package com.example.currencycryptoapp
+
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.currencycryptoapp.presentation.ui.CoinPriceListActivity
+import junit.framework.TestCase
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CoinPriceListActivityEspressoTest {
+    private lateinit var scenario: ActivityScenario<CoinPriceListActivity>
+
+    @Before
+    fun setup() {
+        scenario = ActivityScenario.launch(CoinPriceListActivity::class.java)
+    }
+
+    @Test
+    fun activity_AssertNotNull() {
+        scenario.onActivity {
+            TestCase.assertNotNull(it)
+        }
+    }
+
+    @Test
+    fun activity_IsResumed() {
+        TestCase.assertEquals(Lifecycle.State.RESUMED, scenario.state)
+    }
+
+    @After
+    fun close() {
+        scenario.close()
+    }
+
+
+
+}
